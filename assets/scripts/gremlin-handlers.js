ca4990ed1cfb2abbc553bf478bc2bc2ca1db7a1f
rrmerugu <rrmerugu@gmail.com>
2020-04-17 08:57:31 +0530

    gremlin response handlers updated

--- assets/scripts/gremlin-handlers.js
+++ assets/scripts/gremlin-handlers.js
@@ -1,7 +1,7 @@
 class GremlinResponseHandlers {
 
 
-    convert_property_to_json(property) {
+    convert_vertex_property_to_json(property) {
 
         let _single_prop = property[0]
         if (_single_prop['@type'] !== "g:VertexProperty") {
@@ -20,43 +20,105 @@ class GremlinResponseHandlers {
             throw "Not a vertex error. check if this is of g:Vertex type:: " + JSON.stringify(vtx);
         }
         let d = {};
+        d['_type'] = "g:Vertex";
         let _this = this;
         d.id = vtx['@value'].id['@value'];
         d.label = vtx['@value'].label;
         let properties = vtx['@value'].properties;
-        Object.keys(properties).forEach(function (key) {
-            let property = properties[key];
-            let _ = _this.convert_property_to_json(property)
-            d[key] = _[key]
-        });
+        if (properties) {
+            Object.keys(properties).forEach(function (key) {
+                let property = properties[key];
+                let _ = _this.convert_vertex_property_to_json(property)
+                d[key] = _[key]
+            });
+        }
+
+        return d;
+
+    }
+
+
+    convert_edge_property_to_json(property) {
+
+        let _single_prop = property
+        if (_single_prop['@type'] !== "g:Property") {
+            throw "Not a g:Property error. check if this is of g:Property type:: " + JSON.stringify(property);
+        }
+        let d = {};
+        let value = _single_prop['@value'].value
+        d[_single_prop['@value'].key] = (typeof value === "string") ? value : value['@value']
         return d;
 
     }
 
 
     convert_edge_to_json(edg) {
+        if (edg['@type'] !== "g:Edge") {
+            throw "Not a edge error. check if this is of g:Edge type:: " + JSON.stringify(edg);
+        }
+        let _this = this;
+        let d = {};
+        d['_type'] = "g:Edge";
+        d['label'] = edg['@value']['label'];
+        d['id'] = edg['@value'].id['@value']['relationId'];
+        d['inV'] = edg['@value'].inV['@value'];
+        d['inVLabel'] = edg['@value'].inVLabel;
+        d['outV'] = edg['@value'].outV['@value'];
+        d['outVLabel'] = edg['@value'].outVLabel;
+
+        let properties = edg['@value'].properties;
+
+        if (properties) {
+            Object.keys(properties).forEach(function (key) {
+                let property = properties[key];
+                let _ = _this.convert_edge_property_to_json(property)
+                d[key] = _[key]
+            });
+
+        }
+
+        return d;
 
     }
 
 
+    convert_list_to_json(list_item) {
+
+        if (list_item['@type'] !== "g:List") {
+            throw "Not a List error. check if this is of g:List type:: " + JSON.stringify(list_item);
+        }
+        let _this = this;
+        let items = [];
+        list_item['@value'].forEach(function (item) {
+            let data_list = _this.process_item(item);
+            data_list.forEach(function (datum) {
+                items.push(datum);
+            });
+        })
+        return items;
+
+    }
+
+    process_item(item) {
+        // this is very useful to route to the respective renderers;
+        let _this = this;
+
+        if (item['@type'] === "g:Vertex") {
+            let _ = _this.convert_vertex_to_json(item);
+            return [_];
+        } else if (item['@type'] === "g:Edge") {
+            let _ = _this.convert_edge_to_json(item);
+            return [_];
+        } else if (item['@type'] === "g:List") {
+            console.log("=======items", item);
+            return _this.convert_list_to_json(item);
+        }
+
+    }
+
     process(response) {
         let request_id = response.request_id;
         let data = response.result.data;
-        let items = data['@value'];
-        let _this = this;
-        let nodes = [];
-        items.forEach(function (item) {
-            if (item['@type'] === "g:Vertex") {
-                let d = _this.convert_vertex_to_json(item);
-                d['_type'] = "g:Vertex";
-                nodes.push(d);
-            } else if (item['@type'] === "g:Edge") {
-                let d = _this.convert_edge_to_json(item);
-                d['_type'] = "g:Edge";
-
-                nodes.push(d);
-            }
-        })
-        return nodes
+        return this.convert_list_to_json(data);
     }
 }
