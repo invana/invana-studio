21705b68fd989ebf7e3ee801873a0cbc53431606
rrmerugu <rrmerugu@gmail.com>
2020-04-17 23:54:44 +0530

    refactors

--- assets/ui-components/graph/graph.js
+++ assets/ui-components/graph/graph.js
@@ -1,6 +1,36 @@
 class VertexUtils {
+    constructor(canvas, color_schema) {
+        this.canvas = canvas;
+        this.color_schema = color_schema;
+    }
 
-    add(vertex) {
+    add(vertices) {
+        let _this = this;
+        let node = this.canvas.selectAll(".node")
+            .data(vertices)
+            .enter()
+            .append("g")
+            .attr("class", "node")
+
+
+        node.append("circle")
+            .attr("r", 10)
+            .style("fill", function (d, i) {
+                return _this.color_schema(i);
+            })
+
+        node.append("title")
+            .text(function (d) {
+                return d.id;
+            });
+
+        node.append("text")
+            .attr("dy", -3)
+            .text(function (d) {
+                return d.name + ":" + d.label;
+            });
+
+        return node
 
     }
 
@@ -12,81 +42,14 @@ class VertexUtils {
 
 class EdgeUtils {
 
-    add(edge) {
+    constructor(canvas, color_schema) {
+        this.canvas = canvas;
+        this.color_schema = color_schema;
 
     }
 
+    add(edges) {
 
-    remove(edge) {
-
-    }
-
-}
-
-
-class DataGraphCanvas {
-
-    // Reference: http://bl.ocks.org/fancellu/2c782394602a93921faff74e594d1bb1
-    constructor(html_selector_id) {
-        this.html_selector_id = html_selector_id;
-
-        this.edge_utils = new EdgeUtils();
-        this.vertex_utils = new VertexUtils();
-
-        this.color_schema = d3.scaleOrdinal(d3.schemeCategory10);
-
-        this.canvas = this.setup_canvas(html_selector_id);
-        // canvas_width and canvas_height should be assigned as
-        // soon as possible before any other methods being called.
-        this.canvas_width = this.canvas.attr("width");
-        this.canvas_height = this.canvas.attr("height");
-
-        this.simulation = this.setup_simulation();
-
-    }
-
-    setup_canvas(html_selector_id) {
-        // Per-type markers, as they don't inherit styles.
-        let svg = d3.select(html_selector_id);
-        svg.append('defs').append('marker')
-            .attrs({
-                'id': 'arrowhead',
-                'viewBox': '-0 -5 10 10',
-                'refX': 13,
-                'refY': 0,
-                'orient': 'auto',
-                'markerWidth': 13,
-                'markerHeight': 13,
-                'xoverflow': 'visible'
-            })
-            .append('svg:path')
-            .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-            .attr('fill', '#999')
-            .style('stroke', 'none');
-        return svg
-    }
-
-    setup_simulation() {
-        return d3.forceSimulation()
-            .force("link", d3.forceLink().id(function (d) {
-                return d.id;
-            }).distance(100).strength(1))
-            .force("charge", d3.forceManyBody())
-            .force("center", d3.forceCenter(this.canvas_width / 2, this.canvas_height / 2));
-    }
-
-    add_vertices(vertex) {
-        this.vertex_utils.add(vertex);
-    }
-
-    add_edges(vertex) {
-        this.vertex_utils.add(vertex);
-    }
-
-    draw(vertices, edges) {
-
-        let _this = this;
-        console.log("vertices " + vertices.length + "; edges " + edges.length);
         let link = this.canvas.selectAll(".link")
             .data(edges)
             .enter()
@@ -138,36 +101,142 @@ class DataGraphCanvas {
                 return d.type
             });
 
-        let node = this.canvas.selectAll(".node")
-            .data(vertices)
-            .enter()
-            .append("g")
-            .attr("class", "node")
-            .call(d3.drag()
-                    .on("start", _this.dragstarted)
-                    .on("drag", _this.dragged)
-                //.on("end", dragended)
-            );
+        return [link, edge_paths, edge_labels];
 
-        node.append("circle")
-            .attr("r", 5)
-            .style("fill", function (d, i) {
-                return _this.color_schema(i);
+    }
+
+
+    remove(edge) {
+
+    }
+
+}
+
+
+class DataGraphCanvas {
+
+    // Reference: http://bl.ocks.org/fancellu/2c782394602a93921faff74e594d1bb1
+    constructor(html_selector_id) {
+        this.html_selector_id = html_selector_id;
+
+
+        this.color_schema = d3.scaleOrdinal(d3.schemeCategory10);
+
+        this.canvas = this.setup_canvas(html_selector_id);
+        this.edge_utils = new EdgeUtils(this.canvas, this.color_schema);
+        this.vertex_utils = new VertexUtils(this.canvas, this.color_schema);
+
+        // canvas_width and canvas_height should be assigned as
+        // soon as possible before any other methods being called.
+        this.canvas_width = this.canvas.attr("width");
+        this.canvas_height = this.canvas.attr("height");
+        this.simulation = this.setup_simulation();
+
+    }
+
+    setup_canvas(html_selector_id) {
+        // Per-type markers, as they don't inherit styles.
+        let svg = d3.select(html_selector_id);
+        svg.append('defs').append('marker')
+            .attrs({
+                'id': 'arrowhead',
+                'viewBox': '-0 -5 10 10',
+                'refX': 13,
+                'refY': 0,
+                'orient': 'auto',
+                'markerWidth': 13,
+                'markerHeight': 13,
+                'xoverflow': 'visible'
             })
+            .append('svg:path')
+            .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+            .attr('fill', '#999')
+            .style('stroke', 'none');
+        return svg
+    }
 
-        node.append("title")
-            .text(function (d) {
+    setup_simulation() {
+        return d3.forceSimulation()
+            .force("link", d3.forceLink().id(function (d) {
                 return d.id;
+            }).distance(100).strength(2))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(this.canvas_width / 2, this.canvas_height / 2));
+    }
+
+    add_vertices(vertices) {
+        return this.vertex_utils.add(vertices);
+    }
+
+    add_edges(edges) {
+        return this.edge_utils.add(edges);
+    }
+
+    draw(vertices, edges) {
+
+        let _this = this;
+        console.log("vertices " + vertices.length + "; edges " + edges.length);
+        let node = this.add_vertices(vertices);
+
+
+        node.call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", _this.dragged)
+            //.on("end", dragended)
+        );
+
+        let _ = this.add_edges(edges);
+        let link = _[0];
+        let edge_paths = _[1];
+        let edge_labels = _[2];
+
+
+        function dragstarted(d) {
+            // let _this = this;
+            if (!d3.event.active) {
+                console.log("this.simulation", this);
+                _this.simulation.alphaTarget(0.3).restart()
+            }
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+
+        function ticked() {
+            link
+                .attr("x1", function (d) {
+                    return d.source.x;
+                })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+
+            node
+                .attr("transform", function (d) {
+                    return "translate(" + d.x + ", " + d.y + ")";
+                });
+
+            edge_paths.attr('d', function (d) {
+                return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
             });
 
-        node.append("text")
-            .attr("dy", -3)
-            .text(function (d) {
-                return d.name + ":" + d.label;
-            });
+            edge_labels.attr('transform', function (d) {
+                if (d.target.x < d.source.x) {
+                    let bbox = this.getBBox();
 
-        let ticked = function () {
-            _this.ticked_actual(node, link, edge_paths, edge_labels);
+                    let rx = bbox.x + bbox.width / 2;
+                    let ry = bbox.y + bbox.height / 2;
+                    return 'rotate(180 ' + rx + ' ' + ry + ')';
+                } else {
+                    return 'rotate(0)';
+                }
+            });
         }
 
         this.simulation
@@ -178,57 +247,12 @@ class DataGraphCanvas {
             .links(edges);
     }
 
-    dragstarted(d) {
-        // let _this = this;
-        // if (!d3.event.active) {
-        //     console.log("this.simulation", this);
-        //     this.simulation.alphaTarget(0.3).restart()
-        // }
-        d.fx = d.x;
-        d.fy = d.y;
-    }
 
     dragged(d) {
         d.fx = d3.event.x;
         d.fy = d3.event.y;
     }
 
-    ticked_actual(node, link, edge_paths, edge_labels) {
-        link
-            .attr("x1", function (d) {
-                return d.source.x;
-            })
-            .attr("y1", function (d) {
-                return d.source.y;
-            })
-            .attr("x2", function (d) {
-                return d.target.x;
-            })
-            .attr("y2", function (d) {
-                return d.target.y;
-            });
-
-        node
-            .attr("transform", function (d) {
-                return "translate(" + d.x + ", " + d.y + ")";
-            });
-
-        edge_paths.attr('d', function (d) {
-            return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
-        });
-
-        edge_labels.attr('transform', function (d) {
-            if (d.target.x < d.source.x) {
-                let bbox = this.getBBox();
-
-                let rx = bbox.x + bbox.width / 2;
-                let ry = bbox.y + bbox.height / 2;
-                return 'rotate(180 ' + rx + ' ' + ry + ')';
-            } else {
-                return 'rotate(0)';
-            }
-        });
-    }
 
 }
 
