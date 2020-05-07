14a6d5bb719de550bc0699afe5d78ada9fa91fc9
rrmerugu <rrmerugu@gmail.com>
2020-05-07 21:58:08 +0530

    theme fixes

--- src/components/core/header-nav.jsx
+++ src/components/core/header-nav.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const headerNavigation = {
-    "background": "#1f222f",
-    "padding": "10px 1rem",
-    "boxShadow": "0 2px 1px 0 rgb(58, 58, 58)"
+    "background": "#333333",
+    "padding": "9px 1rem",
+    "boxShadow": "0 2px 1px 0 rgb(58, 58, 58)",
 }
 const headerNavigationH1 = {
     "margin": "0",
