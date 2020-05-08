d302aa060da42d2782bbbba131c514e9b0b35327
rrmerugu <rrmerugu@gmail.com>
2020-05-08 17:37:45 +0530

    console page ui added

--- src/pages/console.jsx
+++ src/pages/console.jsx
@@ -4,6 +4,21 @@ import HeaderNav from "../components/core/header-nav";
 import MainContent from "../components/core/main-content";
 
 
+const textAreaDiv = {
+    "width": "calc(50% - 30px)",
+    "height": "calc(100vh - 42px)"
+}
+const textAreaCls = {
+    "width": "100%",
+    "height": "100%",
+    "background": "#212427",
+    "border": "1px solid #2f2f2f",
+    "resize": "none",
+    "color": "white",
+    "padding": "15px",
+    "fontSize": "16px"
+}
+
 export default class ConsoleView extends React.Component {
 
     constructor(props) {
@@ -19,6 +34,17 @@ export default class ConsoleView extends React.Component {
                 <LeftNav/>
                 <MainContent>
                     <HeaderNav title={this.state.title}/>
+                    <div>
+                        <div style={textAreaDiv}>
+                            <textarea style={textAreaCls} name="" id="" cols="30" rows="10">
+                            </textarea>
+                        </div>
+                        <div className="">
+                            <code>
+                            </code>
+                        </div>
+
+                    </div>
                 </MainContent>
             </div>
         );
