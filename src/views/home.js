72b15535ba4a642b76f2ccafca4357366bb93e6d
rrmerugu <rrmerugu@gmail.com>
2020-06-15 16:44:11 +0530

    styling updates

--- src/views/home.js
+++ src/views/home.js
@@ -105,7 +105,6 @@ export default class HomeView extends GremlinConnectorComponent {
                             }
                         })()}
                     </ErrorBoundary>
-
                 </MainContent>
                 {parentHTML}
             </div>
