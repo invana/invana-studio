4ba11abf285dab58ba22b95c2ec7277dabce4f73
rrmerugu <rrmerugu@gmail.com>
2020-06-18 22:37:24 +0530

    links added to learn section

--- src/core/components/learn.js
+++ src/core/components/learn.js
@@ -1,5 +1,8 @@
 import React from "react";
 import FlyOutUI from "../ui/flyout";
+import {faExternalLinkSquareAlt, faExternalLinkAlt} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 
 export default class LearnFlyOut extends React.Component {
 
@@ -11,7 +14,20 @@ export default class LearnFlyOut extends React.Component {
                       position={"right"}
                       onClose={this.props.onClose}
             >
-                learn section comes here
+
+                <ul>
+                    <li>
+                        <a target={"_blank"} href="https://medium.com/invanalabs/crud-cheatsheet-to-apache-tinkerpop-gremlin-393540cd46ae?source=collection_home---4------1-----------------------">
+                          1. CRUD CheatSheet to Apache TinkerPop Gremlin. <FontAwesomeIcon className={"small"} icon={faExternalLinkSquareAlt} />
+                        </a>
+                    </li>
+                    <li>
+                        <a target={"_blank"} href="https://tinkerpop.apache.org/docs/current/reference/">
+                            2. Apache Tinkerpop Documentation <FontAwesomeIcon className={"small"} icon={faExternalLinkSquareAlt} />
+
+                        </a>
+                    </li>
+                </ul>
             </FlyOutUI>
         )
     }
