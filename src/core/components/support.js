c4b5aef5336c03e095888c95d7a40de717f87c64
rrmerugu <rrmerugu@gmail.com>
2020-06-20 22:14:13 +0530

    support page added

--- src/core/components/support.js
+++ src/core/components/support.js
@@ -1,5 +1,8 @@
 import React from "react";
 import FlyOutUI from "../ui/flyout";
+import {faBook, faSignInAlt} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faMedium} from "@fortawesome/free-brands-svg-icons";
 
 export default class SupportFlyout extends React.Component {
 
@@ -10,7 +13,32 @@ export default class SupportFlyout extends React.Component {
                       display={"block"}
                       position={"right"}
                       onClose={this.props.onClose}
-            />
+            >
+
+
+                <ul className={"vertical list"}>
+                    <li>
+                        <a onClick={() => alert('Its a work in progress')}>
+                            <FontAwesomeIcon icon={faBook}/> End-User Documentation [WIP]</a>
+                    </li>
+                    <li>
+                        <a target={"_blank"} href="https://medium.com/invanalabs">
+                            <FontAwesomeIcon icon={faMedium}/> More Use case blogs on Medium[WIP]</a>
+                    </li>
+                    <li>
+                        <a target={"_blank"} href="https://github.com/invanalabs/graph-explorer/issues">
+                            <FontAwesomeIcon icon={faGithub}/> Bugs, Feature Requests on GitHub
+                        </a>
+                    </li>
+                    <li>
+                        <a onClick={() => alert('Its a work in progress')}>
+                            <FontAwesomeIcon icon={faBook}/> Developer Documentation [WIP]</a>
+                    </li>
+                </ul>
+
+                <p>If you need any support beyond these, please get in touch with me at ravi[ a ]invana.io.</p>
+
+            </FlyOutUI>
         )
     }
 
