4a80fffc7929ae3c03a87a890b73db0481bf2ac3
rrmerugu <rrmerugu@gmail.com>
2020-06-27 23:22:01 +0530

    support page ui updates

--- src/core/components/support.js
+++ src/core/components/support.js
@@ -21,24 +21,11 @@ export default class SupportFlyOut extends React.Component {
                 <ul className={"vertical list"}>
 
                     <li>
-                        <a href={"https://twitter.com/invanalabs"}>
-                            <h4><FontAwesomeIcon icon={faTwitter}/> Twitter</h4>
-                            <p>Stay upto date about the releases, updates, use case blogs, </p>
-                        </a>
-                    </li>
-                    <li>
                         <a onClick={() => alert('Its a work in progress')}>
                             <h4><FontAwesomeIcon icon={faBook}/> Documentation [WIP]</h4>
                             <p>Beginner tutorials, usage and developer documentation.</p>
                         </a>
                     </li>
-                    <li>
-                        <a target={"_blank"} href="https://medium.com/invanalabs">
-                            <h4><FontAwesomeIcon icon={faMedium}/> Medium</h4>
-                            <p>Blogs on latest implementations and more use cases.</p>
-                        </a>
-                    </li>
-
                     <li>
                         <a target={"_blank"} href="https://github.com/invanalabs/graph-explorer/issues">
                             <h4><FontAwesomeIcon icon={faGithub}/> GitHub</h4>
@@ -53,7 +40,19 @@ export default class SupportFlyOut extends React.Component {
                         </a>
                     </li>
 
+                    <li>
+                        <a target={"_blank"} href="https://medium.com/invanalabs">
+                            <h4><FontAwesomeIcon icon={faMedium}/> Medium</h4>
+                            <p>Blogs on latest implementations and more use cases.</p>
+                        </a>
+                    </li>
 
+                    <li>
+                        <a href={"https://twitter.com/invanalabs"}>
+                            <h4><FontAwesomeIcon icon={faTwitter}/> Twitter</h4>
+                            <p>Stay upto date about the releases, updates, use case blogs, </p>
+                        </a>
+                    </li>
                 </ul>
 
                 <p>If you need any support beyond the above,
