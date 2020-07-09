d169af12209c70f56fc3b2650b98b4ae0d41232c
rrmerugu <rrmerugu@gmail.com>
2020-07-09 15:52:28 +0530

    demo url updated

--- src/config.js
+++ src/config.js
@@ -40,4 +40,4 @@ export const linkCurvature = .85;
 export const managementVertexLabel = "InvanaManagement";
 export const historyLocalStorageKey = "GRAPH_EXPLORER_HISTORY";
 export const MAX_HISTORY_COUNT_TO_REMEMBER = 100;
-export const DEMO_URL = "https://www.youtube.com/watch?v=7jdgUOZUtlQ";
+export const DEMO_URL = "https://www.youtube.com/watch?v=SuxC4EH0RCs";
