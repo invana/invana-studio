c0ff2ad42f7fd990d7da8e8f46cdd450d3223eee
rrmerugu <rrmerugu@gmail.com>
2020-07-09 12:06:03 +0530

    support viewlet content changes - order changed to put functional items to the top.

--- src/viewlets/support.js
+++ src/viewlets/support.js
@@ -13,38 +13,41 @@ export default class SupportComponent extends React.Component {
             <div className={"p-10"}>
                 <GEList type={"vertical"}>
 
+
                     <li>
-                        <button   onClick={() => alert('Its a work in progress')}>
-                            <h4><FontAwesomeIcon icon={faBook}/> Documentation [WIP]</h4>
-                            <p>Beginner tutorials, usage and developer documentation.</p>
-                        </button>
-                    </li>
-                    <li>
-                        <a target={"_blank"} rel="noopener noreferrer"  href="https://github.com/invanalabs/graph-explorer/issues">
+                        <a target={"_blank"} rel="noopener noreferrer"
+                           href="https://github.com/invanalabs/graph-explorer/issues">
                             <h4><FontAwesomeIcon icon={faGithub}/> GitHub</h4>
                             <p>Bugs, Feature Requests on GitHub</p>
 
                         </a>
                     </li>
-                    <li>
-                        <button   onClick={() => alert('Its a work in progress')}>
-                            <h4><FontAwesomeIcon icon={faGitter}/> Gitter Community [WIP]</h4>
-                            <p>Chatroom for Community</p>
-                        </button>
-                    </li>
+
 
                     <li>
-                        <a target={"_blank"} rel="noopener noreferrer"  href="https://medium.com/invanalabs">
+                        <a target={"_blank"} rel="noopener noreferrer" href="https://medium.com/invanalabs">
                             <h4><FontAwesomeIcon icon={faMedium}/> Medium</h4>
                             <p>Blogs on latest implementations and more use cases.</p>
                         </a>
                     </li>
 
                     <li>
-                        <a target={"_blank"} rel="noopener noreferrer"  href={"https://twitter.com/invanalabs"}>
+                        <a target={"_blank"} rel="noopener noreferrer" href={"https://twitter.com/invanalabs"}>
                             <h4><FontAwesomeIcon icon={faTwitter}/> Twitter</h4>
                             <p>Stay upto date about the releases, updates, use case blogs, </p>
                         </a>
+                    </li>
+                    <li>
+                        <button onClick={() => alert('Its a work in progress')}>
+                            <h4><FontAwesomeIcon icon={faBook}/> Documentation [WIP]</h4>
+                            <p>Beginner tutorials, usage and developer documentation.</p>
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={() => alert('Its a work in progress')}>
+                            <h4><FontAwesomeIcon icon={faGitter}/> Gitter Community [WIP]</h4>
+                            <p>Chatroom for Community</p>
+                        </button>
                     </li>
                 </GEList>
 
