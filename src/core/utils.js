e017c0d3468aab84090d87434182e4bed4ba0cef
rrmerugu <rrmerugu@gmail.com>
2020-07-12 10:22:20 +0530

    updated LightenDarkenColor order of RGB channels

--- src/core/utils.js
+++ src/core/utils.js
@@ -5,7 +5,7 @@ const gremlinSerializer = new GremlinResponseSerializers();
 
 export function LightenDarkenColor(col, amt) {
 
-    var usePound = false;
+    let usePound = false;
 
     if (col[0] === "#") {
         col = col.slice(1);
@@ -19,17 +19,17 @@ export function LightenDarkenColor(col, amt) {
     if (r > 255) r = 255;
     else if (r < 0) r = 0;
 
-    let g = (num & 0x0000FF) + amt;
+    let g = ((num >> 8) & 0xff) + amt;
 
     if (g > 255) g = 255;
     else if (g < 0) g = 0;
 
-    let b = ((num >> 8) & 0x00FF) + amt;
+    let b = (num & 0xff) + amt;
 
     if (b > 255) b = 255;
     else if (b < 0) b = 0;
 
-    return (usePound ? "#" : "") + ( (r << 16) | g | (b << 8)).toString(16);
+    return (usePound ? "#" : "") + ((r << 16) | (g << 8) | b).toString(16);
 
 }
 
