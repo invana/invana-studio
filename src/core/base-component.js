9ae5514eb8b49a47d0f54047273b0790d21692c0
rrmerugu <rrmerugu@gmail.com>
2020-10-04 09:16:17 +0530

    timer removed from base component

--- src/core/base-component.js
+++ src/core/base-component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {DefaultMaxTimeElapsedWarningInSeconds} from "../config";
+// import {DefaultMaxTimeElapsedWarningInSeconds} from "../config";
 
 
 export default class BaseComponent extends React.Component {
@@ -17,22 +17,11 @@ export default class BaseComponent extends React.Component {
             statusMessage: null,
             isLoading: false,
             loaderElapsedTimer: 0,
-            loaderMaxTimeElapsedError: false,
             loadingMessage: null,
             loadingExtraText: null,
         }
     }
 
-
-    startLoader(message, extraMessage) {
-        this.setState({
-            loadingMessage: message,
-            loadingExtraText: extraMessage,
-            isLoading: true,
-            loaderElapsedTimer: 0
-        })
-        this.startLoaderTimer()
-    }
 
     resetLoader() {
         clearInterval(this.loaderTimerIntervalId);
@@ -45,40 +34,6 @@ export default class BaseComponent extends React.Component {
 
     }
 
-
-    updateTimer(timerCount, isMaxTimeElapsed) {
-        this.setState({loaderElapsedTimer: timerCount, loaderMaxTimeElapsedError: isMaxTimeElapsed});
-    }
-
-    startLoaderTimer() {
-        console.log("loader Timer started")
-        let _this = this;
-        let i = 0;
-        this.loaderTimerIntervalId = setInterval((function () {
-                i += 1;
-
-                console.log("base loader Timer started xyx; i", i);
-                if (_this.state.isLoading === false) {
-                    clearInterval(_this.loaderTimerIntervalId);
-                }
-
-                if (i >= DefaultMaxTimeElapsedWarningInSeconds) {
-                    _this.updateTimer(i, true);
-                } else {
-                    _this.updateTimer(i, false);
-
-                }
-
-            }
-        ), 1000); // check every second.
-    }
-
-    componentWillUnmount() {
-
-        clearInterval(this.loaderTimerIntervalId);
-
-        // clearInterval(this.timer2)
-    }
 
     setStatusMessage(messageText) {
         this.setState({statusMessage: messageText});
