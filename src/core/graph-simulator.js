1b892f0e3f2b5ab8c1b4ad85cf178cad2b6da55d
rrmerugu <rrmerugu@gmail.com>
2020-11-12 13:04:00 +0530

    graph simulation cleaned; collion fixes added

--- src/core/graph-simulator.js
+++ src/core/graph-simulator.js
@@ -16,38 +16,21 @@ export default class GraphSimulator {
     }
 
     generateForceSimulation() {
-        // const defaultLinkLength = this.settings.DEFAULT_LINK_LENGTH;
-        // return d3.forceSimulation()
-        //     .force("link", d3.forceLink()
-        //         .id(linkData => linkData.id)
-        //         .distance(function (d) {
-        //             return d.distance || defaultLinkLength
-        //         })
-        //     )
-        //     .force("charge", d3.forceManyBody().strength(this.settings.FORCE_LAYOUT_NODE_REPULSION_STRENGTH))
-        //     // .force("center", d3.forceCenter())
-        //     .force("x", d3.forceX())
-        //     .force("y", d3.forceY())
-        //
-        //     // .tick(this.settings.FORCE_LAYOUT_ITERATIONS)
-        //     // .on("tick", () => this.onForceSimulationEnd(this))
-        //     .on("end", () => this.onForceSimulationEnd(this))
-        // .stop();
-
         return d3.forceSimulation()
             .force("charge", d3.forceManyBody().strength(this.settings.FORCE_LAYOUT_NODE_REPULSION_STRENGTH))
             .force("link", d3.forceLink().id(d => d.id).distance(this.settings.DEFAULT_LINK_LENGTH))
+            .force("collide", d3.forceCollide(this.settings.NODE_HIT_RADIUS).iterations(10))
+            // .force("center", d3.forceCenter(this.settings.SCREEN_WIDTH / 2, this.settings.SCREEN_HEIGHT / 2))
+
             .force("x", d3.forceX())
             .force("y", d3.forceY())
             .alphaDecay(0.5)
             // .tick(this.settings.FORCE_LAYOUT_ITERATIONS)
-
-            .on("end", () => this.onForceSimulationEnd())
-        // .on("tick", () => this.onForceSimulationEnd(this))
-        //     .tick([this.settings.FORCE_LAYOUT_ITERATIONS])
-
-        // .alphaTarget(0.8);
-
+            .on("end", () => this.onForceSimulationEnd());
+            //.on("tick", () => this.onForceSimulationEnd(this))
+            //.tick([this.settings.FORCE_LAYOUT_ITERATIONS])
+            //.alphaTarget(0.8);
+            //.stop();
     }
 
     addDataToGraphSimulation(vertices2RenderPrepared, edges2RenderPrepared) {
