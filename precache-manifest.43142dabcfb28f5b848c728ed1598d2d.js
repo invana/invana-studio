119d4cc2658a895788e59c9730aaecb6036802a6
Ravi Raja Merugu <rrmerugu@gmail.com>
2020-12-02 16:19:51 +0530

    Updates

---
+++ precache-manifest.43142dabcfb28f5b848c728ed1598d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ca871ace91e1c28426d078d67ce6a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae53e960eba02be4ab67",
+    "url": "/static/css/main.8da9d2e6.chunk.css"
+  },
+  {
+    "revision": "fe706af1726fbe5038d2",
+    "url": "/static/js/2.0918cffd.chunk.js"
+  },
+  {
+    "revision": "058759cacac8e86ea085f7365a0365e3",
+    "url": "/static/js/2.0918cffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae53e960eba02be4ab67",
+    "url": "/static/js/main.0b02f5b9.chunk.js"
+  },
+  {
+    "revision": "036520e2f8c52767c5a1",
+    "url": "/static/js/runtime-main.3dda3d66.js"
+  }
+]);
