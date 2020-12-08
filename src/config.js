d38c8fa89d23285d948cd6dc8f34d913311288a4
Ravi Raja Merugu <rrmerugu@gmail.com>
2020-12-08 17:52:51 +0530

    Version in config changed to alpha

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ export const DefaultMaxTimeElapsedWarningInSeconds = 180;
 // export const DefaultConnectionRetryTimeout = 20;
 
 
-export const VERSION = "pre-beta";
+export const VERSION = "alpha";
 export const ABOUT_TEXT = "Open source visualiser for exploring the graph data.";
 export const CONNECT_URL = "/connect";
 export const REPO_URL = "https://github.com/invanalabs/graph-explorer";
