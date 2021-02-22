28695ea43336b8cd769629ec066748929c4aac6d
Ravi Raja Merugu <rrmerugu@gmail.com>
2021-02-22 19:04:39 +0530

    electron builder fixes added

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ Apache TinkerPop's Gremlin Query language.
 
 **NOTE: Under active development**
 
+## How to use 
+### HTML build 
+
+### Generate electron build
+```shell
+npm run-script electron-dev --scripts-prepend-node-path
+
+```
+
 ## License 
 Apache License 2.0
 
---
+++ public/electron.js
@@ -0,0 +1,26 @@
+const {app, BrowserWindow} = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false,
+        // frame:false,
+        // webPreferences: {nodeIntegration: true}
+    });
+    console.log("IsDev", isDev);
+    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
+
+    mainWindow.loadURL(startURL);
+
+    mainWindow.once('ready-to-show', () => mainWindow.show());
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
