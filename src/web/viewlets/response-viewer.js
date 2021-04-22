bcd4930cdb5a5047b0510778830ad162e8fb9ee3
Ravi Raja Merugu <rrmerugu@gmail.com>
2021-04-22 16:51:26 +0530

    response viewer cleanups

--- src/web/viewlets/response-viewer.js
+++ src/web/viewlets/response-viewer.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Card} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faDesktop} from "@fortawesome/free-solid-svg-icons";
+import {faDesktop, faWindowClose} from "@fortawesome/free-solid-svg-icons";
 
 
 export default class ResponseViewer extends React.Component {
@@ -16,6 +16,10 @@ export default class ResponseViewer extends React.Component {
             <div>
                 <Card className={"p-10 border-0"}>
                     <Card.Header><FontAwesomeIcon icon={faDesktop}/> Response Viewer
+                        <button type="button" onClick={() => this.props.onClose()}
+                                className={"btn btn-link p-0 border-0 fa-pull-right"}><FontAwesomeIcon
+                            icon={faWindowClose}/></button>
+
                     </Card.Header>
                     <Card.Body>
                         <pre>{JSON.stringify(this.props.responseData, null, 4)}
