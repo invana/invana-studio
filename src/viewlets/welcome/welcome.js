3f44ccc36189a0ee31a5f8ec6fd4eee7bec19f63
Ravi Raja Merugu <rrmerugu@gmail.com>
2021-05-06 00:32:52 +0530

    welcome added

---
+++ src/viewlets/welcome/welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Welcome extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Welcome
+            </div>
+        )
+    }
+
+}
--- src/views/explorer/explorer.js
+++ src/views/explorer/explorer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DefaultLayout from "../../ui-components/layout/default-layout";
 import Canvas from "../../viewlets/canvas/canvas";
+import Welcome from "../../viewlets/welcome/welcome";
 
 
 export default class Explorer extends React.Component {
@@ -9,6 +10,7 @@ export default class Explorer extends React.Component {
     render() {
         return (
             <DefaultLayout>
+                <Welcome/>
                 <Canvas />
             </DefaultLayout>
         );
