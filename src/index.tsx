315d97df9c8a45debb75ac6d79f6d6da3e57a010
Ravi Raja Merugu <rrmerugu@gmail.com>
2023-10-29 13:38:07 +0530

    react app added

--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,16 @@
-/*
- * Copyright 2021 Invana
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http:www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'rsuite/styles/index.less';
-import { CustomProvider } from 'rsuite';
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    // useQuery,
-    // gql
-} from "@apollo/client";
-import {STUDIO_SETTINGS} from "./settings";
 
-const client = new ApolloClient({
-    uri: STUDIO_SETTINGS.CONNECTION_URL + "/graphql",
-    cache: new InMemoryCache()
-});
-
-ReactDOM.render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <CustomProvider 
-            // theme="dark"
-            ><App/></CustomProvider>
-        </ApolloProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
