029fd9ee3e6191d02018e87217f6ac48cd01e6eb
Ravi Raja Merugu <rrmerugu@gmail.com>
2023-11-02 13:43:08 +0530

    footer canvas data stats updated

--- src/components/canvas/footer/footer.tsx
+++ src/components/canvas/footer/footer.tsx
@@ -7,17 +7,16 @@ const CanvasFooter = () => {
 
 
     const hoveredElement: any = useSelector((state: RootState)=> state.canvas.hoveredElement);
-    
-    console.log("====hoveredElement", hoveredElement)
-    const canvasDataStats = { nodesCount: 120, edgesCount: 2030 }
+    const canvasData: any = useSelector((state: RootState)=> state.canvas.canvasData);
 
+    console.log("====hoveredElement", hoveredElement)
+ 
     return <div className="canvasFooter" style={{paddingRight: 15}}>
         {
             hoveredElement && <span>{hoveredElement.type}#{hoveredElement.id}[{hoveredElement.group}:{hoveredElement.label}] </span>
         }
-        
         <span style={{ float: "right" }}>
-            <span style={{ marginRight: 15 }}>{canvasDataStats.nodesCount} nodes, {canvasDataStats.edgesCount} relationships in the canvas</span>
+            <span style={{ marginRight: 15 }}>{canvasData.nodes.length} nodes, {canvasData.edges.length} relationships in the canvas</span>
             <span style={{ marginRight: 15 }}>took 300ms to query</span>
         </span>
     </div>
